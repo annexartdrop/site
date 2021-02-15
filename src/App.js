d250c19ffd1cdf0f8663ba58b55373eccce0827b
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-02-14 17:06:13 -0800

    setup about page routes

--- src/App.js
+++ src/App.js
@@ -6,20 +6,16 @@ import {
   withRouter,
 } from "react-router-dom";
 import Home from "./pages/home";
+import About from "./pages/about";
 
 const App = withRouter(
   class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-      };
-    }
-
     render() {
       return (
         <div>
             <Switch>
-              <Route path="/" exact component={Home}></Route>
+              <Route path="/" exact component={Home}/>
+              <Route path="/about" component={About}/>
             </Switch>
         </div>
       );
