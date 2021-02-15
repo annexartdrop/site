a8cff326c9948374f254a06e7dbb877620084f24
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-02-14 16:45:27 -0800

    browser router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from "react-router-dom";
 import Home from "./pages/home";
 
-export default function App() {
-  return (
-    <div>
-      <Home/>
-    </div>
-  );
+const App = withRouter(
+  class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+      };
+    }
+
+    render() {
+      return (
+        <div>
+            <Switch>
+              <Route path="/" exact component={Home}></Route>
+            </Switch>
+        </div>
+      );
+    }
+  }
+);
+
+class RoutedApp extends Component {
+  render() {
+    return (
+      <Router basename="/">
+        <App />
+      </Router>
+    );
+  }
 }
+
+export default RoutedApp;
