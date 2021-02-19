90ab3312f95f0f71b8bf477a8adb7bd16b65a6cb
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-02-18 19:16:49 -0800

    adding in other text

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -8,9 +8,15 @@ export default function About() {
     <React.Fragment>
       <OtherNav/>
       <div className={style.aboutContainer}>
-        <h2>About</h2>
-        <p>This project is sustained &amp; driven by the community. Inspired by @sentientmoss’ @wabashartdrop project, the Annex holds the same mission.</p>
-        <p>By using recycled newspaper stands (either donated by companies or scouted by city affiliates) “we are able to reduce trash on the street while simultaneously encouraging visual art and making it an accessible project for anyone wanting to get involved regardless of their financial situation.” </p>
+        <section>
+          <h2>About</h2>
+          <p>This project is sustained &amp; driven by the community. Inspired by @sentientmoss’ @wabashartdrop project, the Annex holds the same mission.</p>
+          <p>By using recycled newspaper stands (either donated by companies or scouted by city affiliates) “we are able to reduce trash on the street while simultaneously encouraging visual art and making it an accessible project for anyone wanting to get involved regardless of their financial situation.” </p>
+        </section>
+        <section>
+          <h2>we hate autocorrect and love aave &amp; pidgin creole.</h2>
+          <p>Yes, the “grammar” on this site is not perfect english. it should not be. we encourage & embrace the language you grew up with. for us, its pidgin creole. We know the effects of “professionalism” on language erasure. when interacting with this space, we encourage you to use whatever language you’re comfortable with.</p>
+        </section>
       </div>
       <Footer/>
     </React.Fragment>
