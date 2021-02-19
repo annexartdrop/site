904387d252eedfb6ca0eec247a9e84a2d5640b19
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-02-18 19:00:34 -0800

    adding in about text

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -10,7 +10,8 @@ export default function About() {
       <OtherNav/>
       <div className={style.aboutContainer}>
         <h2>About</h2>
-        <p>{store.aboutPage.text}</p>
+        <p>This project is sustained &amp; driven by the community. Inspired by @sentientmoss’ @wabashartdrop project, the Annex holds the same mission.</p>
+        <p>By using recycled newspaper stands (either donated by companies or scouted by city affiliates) “we are able to reduce trash on the street while simultaneously encouraging visual art and making it an accessible project for anyone wanting to get involved regardless of their financial situation.” </p>
       </div>
       <Footer/>
     </React.Fragment>
