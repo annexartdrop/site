6af600b0868f1bc69ec5cf09cde05b8624026ab7
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-02-18 18:32:14 -0800

    adding in the floating hearts

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,29 @@
+import React, { useEffect, useRef } from "react";
 import style from "./index.module.scss"
 import Ticker from "../../components/ticker";
 import { ReactComponent as Hearts } from "../../images/peepoo.svg";
 import { ReactComponent as Location } from "../../images/location.svg";
 import HomeNav from "../../components/nav/homenav";
+import gsap from "gsap";
 
 export default function Home() {
+  let heartRef = useRef(null);
+  var timeline = gsap.timeline({ repeat: -1 });
+
+  useEffect(() => {
+    timeline
+      .to(heartRef.current, { duration: 5, y: "-=20", ease: "sine" })
+      .to(heartRef.current, { duration: 5, y: "+=20", ease: "sine" });
+  }, [timeline]);
+
   return (
     <div>
       <HomeNav/>
       <div className={style.heroContainer}>
         <div className={style.heroCenter}>
-          <Hearts className={style.hearts}/>
+          <div ref={heartRef}>
+            <Hearts className={style.hearts}/>
+          </div>
           <h1>annex art drop</h1>
           <div className={style.locationContainer} onClick={(event) => {event.preventDefault(); window.open("https://www.google.com/maps/place/534+SW+Jackson+St,+Portland,+OR+97201/@45.5089016,-122.6833253,3a,75y,205.77h,71.77t/data=!3m6!1e1!3m4!1s-R9F--u_ipRkFKmxde0R1g!2e0!7i13312!8i6656!4m5!3m4!1s0x54950a143349daff:0x856d3639c1656c47!8m2!3d45.5087612!4d-122.683622")}}>
             <Location id={style.locationIcon}/>
