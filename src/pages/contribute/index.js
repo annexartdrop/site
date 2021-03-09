fb9d0b4b1301cfc85ad7740769c66a26ddbf3cce
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-03-08 20:48:55 -0800

    some temporary language for the contribute

--- src/pages/contribute/index.js
+++ src/pages/contribute/index.js
@@ -14,8 +14,8 @@ export default function Contribute() {
           <p>By using recycled newspaper stands (either donated by companies or scouted by city affiliates) “we are able to reduce trash on the street while simultaneously encouraging visual art and making it an accessible project for anyone wanting to get involved regardless of their financial situation.” </p>
         </section>
         <section>
-          <h2>we hate autocorrect and love aave &amp; pidgin creole.</h2>
-          <p>Yes, the “grammar” on this site is not perfect english. At Annex, we encourage &amp; embrace the language you grew up with. For us, its pidgin creole. We know the effects of “professionalism” on language erasure. When interacting with this space, we encourage you to use whatever language you’re comfortable with.</p>
+          <h2>For Your Visit</h2>
+          <p>We suggest bringing along a few things!</p>
         </section> 
       </div>
       <Footer/>
