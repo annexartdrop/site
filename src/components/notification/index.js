3ebbdb4a2a00b6b448eae02d1353a8fd98938d63
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-03-25 22:44:16 -0700

    setting up the notification component

---
+++ src/components/notification/index.js
@@ -0,0 +1,12 @@
+import style from "./index.module.scss";
+import React from "react";
+
+const Notification = (props) => {
+  return (
+    <React.Fragment>
+      <h1 className={style.notification}>Notification</h1>
+    </React.Fragment>
+  );
+}
+
+export default Footer;
